ceb07bea9a258710ae35fb3ddc7047ac9720dead
Muhammet Ramazan <ramazan.ykms@gmail.com>
2022-08-10 21:04:32 +0300

    user login

--- app.py
+++ app.py
@@ -3,32 +3,37 @@ from email import message
 from os import sendfile
 from turtle import onclick
 from unicodedata import name
-from flask import Flask, request, render_template , make_response
+from flask import Flask, request, render_template, make_response
 import numpy as np
 from smtplib import SMTP
 import json
 import sendMail
 import pandas as pd
-from openpyxl import Workbook,load_workbook
+from openpyxl import Workbook, load_workbook
 import io
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def Definitions():
     return render_template('anasayfa.html')
 
 
+wrongPass = "Şifreniz yanlış"
+wrongUser = "User yanlış"
 
 
-
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload():
-    #Formdaki verileri teker teker bir değişkene atadık.
-    firstname= request.form["firstname"]
-    lastname = request.form["lastname"]
-    tcidentityno= request.form["tcidentityno"]
-    email= request.form["email"]
+    readUser = open('metaUser.json')
+    data = json.load(readUser)
+    metaUser = data['users']
+    # Formdaki verileri teker teker bir değişkene atadık.
+    username = request.form["username"]
+    password = request.form["password"]
+    tcidentityno = request.form["tcidentityno"]
+    email = request.form["email"]
     department = request.form["department"]
     permissiontype = request.form["permissiontype"]
     permissionstart = request.form["permissionstart"]
@@ -36,34 +41,39 @@ def upload():
     startingwork = request.form["startingwork"]
     permissionperiod = request.form["permissionperiod"]
     radioButton = request.form["radio"]
-    
-    
-    #Atama Yaptığımız değişkenleri burada bir dictionary haline getiriyoruz.
-    dictionary = {
-    'Firma Adı' :[radioButton],
-    'Adı':[firstname],
-    'Soyadı':[lastname],
-    'TC No':[tcidentityno],
-    'Email':[email],
-    'Çalıştığı Bölüm':[department],
-    'İzin Türü':[permissiontype],
-    'İzin Başlangıç':[permissionstart],
-    'İzin Bitiş':[permissionfinished],
-    'İşe Başlama':[startingwork],
-    'İzin Süresi':[permissionperiod],
-     }
-    #Excele Textbox'tan alınana verileri kaydetme
-    df = pd.DataFrame(dictionary)
-    # # df = df.append(dictionary,ignore_index=True)
-    writer = pd.ExcelWriter('pythondeneme.xlsx',engine='xlsxwriter')
-    df.to_excel(writer,sheet_name='Sheet1',index=True)
-    writer.save()
-    # #Mail Gönderme Fonksiyonu
-    sendMail.sendMail()
-    return dictionary
 
-
-    
-
-
-    
+    i = 0
+    userList = len(metaUser)
+    while i < userList:
+        user = metaUser[i]
+        if user['user'] == username:
+            if user['pass'] == password:
+                # Atama Yaptığımız değişkenleri burada bir dictionary haline getiriyoruz.
+                dictionary = {
+                    'Firma Adı': [radioButton],
+                    'Kullanıcı Adı': [username],
+                    'Şifre': [password],
+                    'TC No': [tcidentityno],
+                    'Email': [email],
+                    'Çalıştığı Bölüm': [department],
+                    'İzin Türü': [permissiontype],
+                    'İzin Başlangıç': [permissionstart],
+                    'İzin Bitiş': [permissionfinished],
+                    'İşe Başlama': [startingwork],
+                    'İzin Süresi': [permissionperiod],
+                }
+                # Excele Textbox'tan alınana verileri kaydetme
+                df = pd.DataFrame(dictionary)
+                # # df = df.append(dictionary,ignore_index=True)
+                writer = pd.ExcelWriter(
+                    'pythondeneme.xlsx', engine='xlsxwriter')
+                df.to_excel(writer, sheet_name='Sheet1', index=True)
+                writer.save()
+                # #Mail Gönderme Fonksiyonu
+                sendMail.sendMail()
+                return dictionary
+            else:
+                return wrongPass
+        else:
+            return wrongUser
+    i += 1
